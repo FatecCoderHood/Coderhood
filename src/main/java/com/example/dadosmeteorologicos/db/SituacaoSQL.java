02766650070e49090796622cc34f6543600dedc0
Michel Momose <michelmomose@gmail.com>
2024-05-13 15:18:43 -0300

    Script SQL modificado para ignorar valores 'null' e 'suspeito'

--- src/main/java/com/example/dadosmeteorologicos/db/SituacaoSQL.java
+++ src/main/java/com/example/dadosmeteorologicos/db/SituacaoSQL.java
@@ -24,9 +24,9 @@ public class SituacaoSQL extends IniciaBanco {
             if(conn != null){
                 // Query para buscar os registros 
                 String sql = "SELECT r1.* FROM registro r1 " +
-                "JOIN (SELECT tipo, siglaCidade, MAX(id) AS maxId FROM registro GROUP BY tipo, siglaCidade) r2 " +
+                "JOIN (SELECT tipo, siglaCidade, MAX(id) AS maxId FROM registro WHERE suspeito = false AND valor IS NOT NULL AND tipo IS NOT NULL AND siglaCidade IS NOT NULL GROUP BY tipo, siglaCidade) r2 " +
                 "ON r1.tipo = r2.tipo AND r1.siglaCidade = r2.siglaCidade AND r1.id = r2.maxId " +
-                "WHERE r1.tipo IN ('temperaturaMedia', 'umidadeMedia', 'VelVento', 'dirVento', 'chuva')";
+                "WHERE r1.tipo IN ('temperaturaMedia', 'umidadeMedia', 'VelVento', 'dirVento', 'chuva') AND r1.suspeito = false AND r1.valor IS NOT NULL AND r1.tipo IS NOT NULL AND r1.siglaCidade IS NOT NULL";
                 Statement stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery(sql);
                 // Adiciona os registros na lista
