6f5f43bc3a27f2506ad7b0be428863c7cbd8f265
Cesar Pelogia <pelogia.cesar@hotmail.com>
2024-06-04 13:05:25 -0300

    Iniciado construção dos testes #71

--- src/main/java/com/example/dadosmeteorologicos/Services/EstacaoService.java
+++ src/main/java/com/example/dadosmeteorologicos/Services/EstacaoService.java
@@ -1,5 +1,6 @@
 package com.example.dadosmeteorologicos.Services;
 
+import java.sql.Connection;
 import java.util.List;
 
 import com.example.dadosmeteorologicos.db.EstacaoSQL;
@@ -7,44 +8,48 @@ import com.example.dadosmeteorologicos.model.Estacao;
 
 public class EstacaoService {
 
+   private EstacaoSQL banco;
+
+   public EstacaoService(){
+      this.banco = new EstacaoSQL();
+   }
+
+   public EstacaoService(Connection conn){
+      this.banco = new EstacaoSQL(conn);
+   }
+
    public List<Estacao> buscaEstacao(){
-      EstacaoSQL estacaoSQL = new EstacaoSQL();
-      List<Estacao> listaEstacao = estacaoSQL.buscaEstacaoBanco();
-      estacaoSQL.fecharConexao();
+      List<Estacao> listaEstacao = banco.buscaEstacaoBanco();
+      banco.fecharConexao();
       return listaEstacao;
    }
 
    public Boolean deletarEstacao(int id, String numero){
-      EstacaoSQL estacaoSQL = new EstacaoSQL();
-      estacaoSQL.deletarEstacaoBanco(id, numero);
-      estacaoSQL.fecharConexao();
+      banco.deletarEstacaoBanco(id, numero);
+      banco.fecharConexao();
       return true;
    }
 
    public void adicionarNovaEstacao(String numeroNovaEstacao, String siglaCidadeNovaEstacao){
       siglaCidadeNovaEstacao = siglaCidadeNovaEstacao.toUpperCase();
-      EstacaoSQL estacaoSQL = new EstacaoSQL();
-      estacaoSQL.adicionarEstacaoBanco(numeroNovaEstacao, siglaCidadeNovaEstacao);
-      estacaoSQL.fecharConexao();
+      banco.adicionarEstacaoBanco(numeroNovaEstacao, siglaCidadeNovaEstacao);
+      banco.fecharConexao();
    }
 
    public Boolean numeroEstacaoValido(String numero){
-      EstacaoSQL estacaoSQL = new EstacaoSQL();
-      Boolean siglaValida = estacaoSQL.numeroEstacaoValido(numero);
-      estacaoSQL.fecharConexao();
+      Boolean siglaValida = banco.numeroEstacaoValido(numero);
+      banco.fecharConexao();
       return siglaValida;
    }
 
    public Boolean siglaCidadeExiste(String sigla){
       sigla = sigla.toUpperCase();
-      EstacaoSQL estacaoSQL = new EstacaoSQL();
-      Boolean siglaValida = estacaoSQL.siglaCidadeExiste(sigla);
-      estacaoSQL.fecharConexao();
+      Boolean siglaValida = banco.siglaCidadeExiste(sigla);
+      banco.fecharConexao();
       return siglaValida;
    }
 
    public void atualizarEstacao(int id, Estacao estacao){
-      EstacaoSQL banco = new EstacaoSQL();
       banco.atualizarEstacaoBanco(id, estacao);
       banco.fecharConexao();
    }
